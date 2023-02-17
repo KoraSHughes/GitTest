750c304ff692a38208373de5ec009baf31e63af5
Kora Hughes <korashughes@gmail.com>
2023-02-17 16:38:41 -0500

    GameManager Start

---
+++ BulletHell/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Game Design -- Bullet Hell Project
+by: Kora Hughes, 
+
+*/
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
